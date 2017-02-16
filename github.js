dddc356cc6d5307e7cef3e9fb2a0f319720fa2e6
Ka-Ping Yee <ping@zesty.ca>
2017-02-16 13:13:34 -0800

    Tighten spacing.

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Fix formatting
 // @author       Ping
 // @match        https://github.com/*
@@ -15,6 +15,26 @@
     var style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = `
+
+/* Reduce height of repository search bar */
+.header {
+  padding: 6px 0;
+}
+
+/* Reduce spacing around page heading and repo heading */
+.pagehead {
+  padding-top: 12px;
+}
+.pagehead, .repohead {
+  margin-bottom: 12px;
+}
+
+/* Reduce height of tabs */
+a.reponav-item, .tabnav-tab {
+  line-height: 16px;
+}
+
+/* Reduce line spacing in code */
 .blob-num, .blob-code-inner {
   font-size: 11px;
 }
@@ -24,6 +44,27 @@
 }
 .add-line-comment {
   height: 18px;
+}
+
+/* Reduce vertical padding in file header */
+.file-header {
+  padding: 0 10px;
+}
+.btn-sm {
+  line-height: 17px;
+}
+
+/* Reduce space between files */
+.files-next-bucket .file {
+  margin-bottom: 16px;
+}
+
+/* Reduce padding above diff summary bar */
+.pr-toolbar {
+  height: 50px;
+}
+.diffbar {
+  padding-top: 10px;
 }
 `;
     head.appendChild(style);
